ab72c937da0fa87759ff1ea6cac0ccc1141e10ec
tootootooyaman <toyama.a.ab@gmail.com>
2021-07-14 19:17:15 +0900

    シンプルなポートフォリオを作成

---
+++ main.js
@@ -0,0 +1,30 @@
+const counter = document.getElementById("counter");
+const plus = document.getElementById("plus");
+const display = document.getElementById("display");
+let number = 0;
+plus.onclick = function () {
+  number += 1;
+  counter.textContent = number;
+  if (isPrimeNumber(number)) {
+    display.textContent = "👀❗️素数😍😍😍😍";
+  } else {
+    display.textContent = "👀　素数じゃない...";
+  }
+  if (number > 50 && number <= 100) {
+    display.textContent += "...暇なの？";
+  }
+  if (number > 100) {
+    display.textContent += "...ばかなの？？";
+  }
+};
+const isPrimeNumber = function (n) {
+  if (n == 1) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+};
